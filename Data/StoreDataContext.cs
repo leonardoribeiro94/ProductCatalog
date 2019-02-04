b977a5a3cb72a4be741274d3b719b65d1384a9e3
Leonardo <leonardostudent94@hotmail.com>
2019-02-04 00:08:24 -0200

    first commit

---
+++ Data/StoreDataContext.cs
@@ -0,0 +1,24 @@
+
+using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Data.Maps;
+using ProductCatalog.Models;
+
+namespace ProductCatalog.Data
+{
+    public class StoreDataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost\sqlexpress;Database=prodcat;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductMap());
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+        }
+    }
+}
